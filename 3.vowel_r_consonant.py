087ad05010257c8267186cc9214aa2b8d8ed8179
Prasanth4u <52313964+Prasanth4u@users.noreply.github.com>
2019-06-28 15:23:47 +0530

    Update 3.vowel_r_consonant.py

--- 3.vowel_r_consonant.py
+++ 3.vowel_r_consonant.py
@@ -2,9 +2,12 @@ val=input("")
 vow=['a','e','i','o','u','A','E','I','O','U']
 cons=['B','T','G','H','b','t','g','h']
 try:
+  val=str(val)
   if val in vow:
    print("Vowel")
   elif val in cons:
    print("Consonant")
+  else:
+   print("invalid")
 except ValueError:
    print("invalid")

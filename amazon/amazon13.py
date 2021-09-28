755270e04903df53526fe651bc438fb1be233a00
Prasanth P <52313964+Prasanth4u@users.noreply.github.com>
2021-09-28 10:42:33 +0530

    Update amazon13.py

--- amazon/amazon13.py
+++ amazon/amazon13.py
@@ -1,16 +1,12 @@
 """
-You are given a string ‘s’.Your task is to print the maximum length of longest palindrome present in string.
-
-Input Description:
-You are given string ‘s’
-
-Output Description:
-Print length of longest palindrome in string
-
-Sample Input :
-abcb
-Sample Output :
-3
+Given 2 strings S1 and S2. Find if String2 is substring of String1. If it is print the index of the first occurrence. else print -1.
+Input Size : 1<= N <= 100000
+Sample Testcases :
+1)INPUT
+test123string
+123
+OUTPUT
+4
 """
 s=str(input())
 sKey=str(input())
